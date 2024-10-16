2a64960154d89a17f422b5ec598cc52a1b60d9e3
LEvik1 <128704968+LEvik1@users.noreply.github.com>
2024-10-16 18:51:07 +0500

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -25,16 +25,69 @@ strelka.addEventListener('click', ()=>{
     img_2.classList.toggle('img_sec2_show')
     header.classList.toggle('header_show')
 })
+let sec_1 = docum.querySelector(".sec_1")
+let sec_2 = docum.querySelector(".sec_2")
+let sec_3 = docum.querySelector(".sec_3")
+let foot = docum.querySelector("footer")
+let pos_sec1 = sec_1.offsetTop;
+let pos_sec2 = sec_2.offsetTop;
+let pos_sec3 = sec_3.offsetTop;
+let pos_foot = foot.offsetTop;
+let a_1_scr = docum.querySelector(".sec1_scr")
+let a_2_scr = docum.querySelector(".sec2_scr")
+let a_3_scr = docum.querySelector(".sec3_scr")
+let a_4_scr = docum.querySelector(".foot_scr")
 
+// a_1_scr.addEventListener('click', (event)=>{
+//     event.preventDefault() // ссылка перестает перекидывать или обновлять страницу
 
+//     window.scrollTo({top: pos_sec1  , left : 0, behavior: 'smooth'})
+// })
+a_1_scr.addEventListener('click', jakor);
+a_2_scr.addEventListener('click', jakor_2);
+a_3_scr.addEventListener('click', jakor_3);
+a_4_scr.addEventListener('click', jakor_4);
+function jakor(event){
+    event.preventDefault() // отключение обновления
+    window.scrollTo({
+        top: pos_sec1,// на сколько вниз он должен прокрутиться 
+        left: 0, 
+        behavior: "smooth" // планость 
+    })
+}
+function jakor_2(event){
+    event.preventDefault() // отключение обновления
+    window.scrollTo({
+        top: pos_sec2,// на сколько вниз он должен прокрутиться 
+        left: 0, 
+        behavior: "smooth" // планость 
+    })
+}
+function jakor_3(event){
+    event.preventDefault() // отключение обновления
+    window.scrollTo({
+        top: pos_sec3,// на сколько вниз он должен прокрутиться 
+        left: 0, 
+        behavior: "smooth" // планость 
+    })
+}
+function jakor_4(event){
+    event.preventDefault() // отключение обновления
+    window.scrollTo({
+        top: pos_foot,// на сколько вниз он должен прокрутиться 
+        left: 0, 
+        behavior: "smooth" // планость 
+    })
+}
+let svt = docum.querySelector(".Svetlana")
 
-
-
-
-
-
-
-
-
-
-
+window.onresize= ()=> {
+    if(window.pageYOffset > 300){
+    svt.classList.toggle("svt_show")
+        
+    }else {
+        alert("Ошибка")
+        console.log("Ошибка");
+        
+    }
+}
